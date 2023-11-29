88b2f7d042ea27569d9128bd133f5707868f2d2e
Pujrani Swain <152247854+swainpuja07@users.noreply.github.com>
2023-11-29 04:22:13 +0000

    File added

---
+++ 01-Basic/Test.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.log("Pujarani Swain");
+/* 1.Javascript is a high level scripting programming language used on devloping web and mobile application efficiently.
+2.we use javascript in both client side as well as server side language.
+3.we use javascript for devloping logics in our code.
+4.Javascript was devloped in 1997.
+5.Now these days we used the ES6 version of javascript .It was started on 2015 and Now it's the latest version of js.
+    ES6-Echma Script 6(Vanilla javascript)
+*/
