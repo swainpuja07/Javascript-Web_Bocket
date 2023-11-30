58abe9739a23f4101d5bb07fff20badf27fb8171
Pujrani Swain <152247854+swainpuja07@users.noreply.github.com>
2023-11-30 05:29:19 +0000

    File added

---
+++ 01-Basic/VariableSummary.js
@@ -0,0 +1,14 @@
+/* 1. In our javascript there are 2 types variable is present.
+    a.primitive:- string,number,Bigint,Symbol,null,Boolean,Undefined.
+    b.Non-primitive/reference:-
+        1.array:- const cars=["BMW","Audi","Lambo"];
+        2.object:-let myobj={
+                            name:"Puajrani";
+                            age:21;
+                            City:"BBSR";
+        =>Object is a key value pair.
+        }
+    c.Function:- const myfunction=function(){
+                 console.log("Hello World")
+                    }
+*/
